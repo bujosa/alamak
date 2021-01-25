5b16d733cc17b023abe7578174cd3eff4b2ec049
bujosa <davidbujosa@gmail.com>
2021-01-25 16:30:50 -0400

    added index html test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 
 // Servidor para los sockets
 const server = require('http').createServer(app);
@@ -6,7 +8,11 @@ const server = require('http').createServer(app);
 // Configuracion del socket server
 const io = require('socket.io')(server);
 
-io.on('connection', () => { /* ... */});
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', () => { 
+    console.log('Cliente conectado!');
+});
 
 server.listen(3000, () => {
     console.log("Server corriendo en el puerto 3000");
