850803921801fbf8047422d7583928c8d633e3eb
bujosa <davidbujosa@gmail.com>
2021-01-25 22:44:02 -0400

    Finished emit messages

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', () => { 
-    console.log('Cliente conectado!');
+io.on('connection', (socket) => {
+    socket.emit('mensaje-bienvenida', {
+        msg: 'Bienvenido al server',
+        date: new Date()
+    });
+
+    socket.on('message-to-server', (data) => {
+        console.log(data);
+        io.emit('message-from-server', data);
+        // socket is individual 
+        // io is global
+    });
 });
+
 
 server.listen(3000, () => {
     console.log("Server corriendo en el puerto 3000");
