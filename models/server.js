b20a11e67d41ba383044f44d9048f6eb46578a3e
bujosa <davidbujosa@gmail.com>
2022-01-24 20:52:21 +0100

    Traslate log

--- models/server.js
+++ models/server.js
@@ -1,42 +1,43 @@
-const express = require('express');
-const http = require('http');
-const sockerio = require('socket.io');
-const path = require('path');
-const Sockets = require('./sockets');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const sockerio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+const cors = require("cors");
 
 class Server {
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
-        this.server = http.createServer(this.app);
-        this.io = sockerio(this.server, { /* Configurations */ });
-    }
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.server = http.createServer(this.app);
+    this.io = sockerio(this.server, {
+      /* Configurations */
+    });
+  }
 
-    middlewares() {
-        this.app.use(express.static(path.resolve(__dirname, '../public')));
+  middlewares() {
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
 
-        // CORS
-        this.app.use(cors());
-    }
+    // CORS
+    this.app.use(cors());
+  }
 
-    sockerConfigurations() {
-        new Sockets(this.io);
-    }
+  sockerConfigurations() {
+    new Sockets(this.io);
+  }
 
-    execute() {
+  execute() {
+    // Init middlewares
+    this.middlewares();
 
-        // Init middlewares
-        this.middlewares();
+    // Init Sockets
+    this.sockerConfigurations();
 
-        // Init Sockets
-        this.sockerConfigurations();
-
-        // Init server
-        this.server.listen(this.port, () => {
-            console.log("Server corriendo en el puerto: ", this.port);
-        });
-    }
+    // Init server
+    this.server.listen(this.port, () => {
+      console.log("Server running in this port: ", this.port);
+    });
+  }
 }
 
 module.exports = Server;
